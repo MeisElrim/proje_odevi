181a381f98d7bc3f3e283a0719495c5158692e98
MeisElrim <105595218+MeisElrim@users.noreply.github.com>
2022-05-15 20:52:18 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function validate(){
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if (username == "MEYS ELRİM" && password == "G211210577" ) {
+        return true
+    } else {
+        alert(" HATALI GİRİŞ !")
+        return false;
+    }
+    
+}
+
+
