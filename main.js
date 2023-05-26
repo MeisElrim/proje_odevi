536a4f4e1fb5553f0e70470354b034937a3cac3e
MeisElrim <105595218+MeisElrim@users.noreply.github.com>
2023-05-26 17:02:49 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,22 +8,3 @@ function logIn() {
         alert("Not Correct")
     }
 }
-
-
-//ilgi//
-//const url = 'https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr';
-//const options = {
-	//method: 'GET',
-	//headers: {
-	//	'X-RapidAPI-Key': '43f19862edmsh252bf7af851a1bcp17ca5cjsn962a66c723ec',
-	//	'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-	//}
-//};
-
-//try {
-//	const response = await fetch(url, options);
-//	const result = await response.text();
-//	console.log(result);
-//} catch (error) {
-//	console.error(error);
-//}
